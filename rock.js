a8be7a7f67af1a80e5555cf1a3337a5eb41bc602
jdorr <jdor1696@gmail.com>
2022-01-27 10:27:03 -0500

    update1

--- rock.js
+++ rock.js
@@ -1,6 +1,8 @@
 
 
-
+document.addEventListener("DOMContentLoaded", function(event) {
+     // Your code to run since DOM is loaded and ready
+ 
 
 
 
@@ -26,77 +28,88 @@ function compareFunction(){
          playerInput == "paper" && rresult =="Rock"||
          playerInput == "scissors" && rresult =="Paper"){
      
-          alert("winner!");
+          txtbocks.innerText = "winner"
      }
      else if (playerInput =="rock" && rresult =="Rock" ||
               playerInput =="paper" && rresult == "Paper" ||
               playerInput =="scissors" && rresult == "Scissors"){
-          alert("Tie");
+               txtbocks.innerText = "Tie"
     
      }
      else if (playerInput=="rock" && rresult =="Paper"||
               playerInput=="paper" && rresult =="Scissors"||
               playerInput=="scissors" && rresult =="Rock"){
-                   alert("Loster!")
+               txtbocks.innerText = "LOSSER"
               }
 }
    
 
-
+console.log(1 === 1)
 
 
 
   
 
-function clickEvent1(){
-     playerInput = "rock";
-     console.log(playerInput);
-     computerPlay();
-     compareFunction()
+// function clickEvent1(){
+//      playerInput = "rock";
+//      console.log(playerInput);
+//      computerPlay();
+//      compareFunction()
 
-}
+// }
          
-function clickEvent2(){
-     playerInput = "paper";
-     console.log(playerInput);
-     computerPlay();
+// function clickEvent2(){
+//      playerInput = "paper";
+//      console.log(playerInput);
+//      computerPlay();
+//      compareFunction()
+
+// }
+
+// function clickEvent3(){
+//      playerInput = "scissors";
+//      console.log(playerInput);
+//      computerPlay();
+//      compareFunction()
+
+// }
+
+const btn1 = document.querySelector("#b1")
+
+const btn2 = document.querySelector("#b2")
+
+const btn3 = document.querySelector("#b3")
+const txtbocks = document.querySelector("#textboxt")
+
+
+
+
+
+
+btn1.addEventListener('click', () => {
+     playerInput = "rock"
+     computerPlay()
      compareFunction()
+     
+});
 
-}
 
-function clickEvent3(){
-     playerInput = "scissors";
-     console.log(playerInput);
-     computerPlay();
+btn2.addEventListener('click', () => {
+     playerInput = "paper"
+     computerPlay()
      compareFunction()
+});
 
-}
-
-
-
-
-
-
-
+btn3.addEventListener('click', () => {
+     playerInput = "scissors"
+     computerPlay()
+     compareFunction()
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
