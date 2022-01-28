02f33e38839c29a1f9de6dc9147bfc372b619786
jdorr <jdor1696@gmail.com>
2022-01-28 14:32:38 -0500

    sound effects

--- rock.js
+++ rock.js
@@ -1,20 +1,20 @@
 
 
-document.addEventListener("DOMContentLoaded", function(event) {
-     // Your code to run since DOM is loaded and ready
- 
 
-  
-   let count = 0;
 
-function checker(){
-     if (count === 5){
-          alert("chcker")
-     }
+
+function gtest(){
+     alert("test")
 }
 
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+     // Your code to run since DOM is loaded and ready
+  
 
 
      
@@ -31,7 +31,7 @@ function computerPlay(){
     else if (result == 3){
          rresult = "Scissors";
     }
-    console.log(rresult);
+    
     
 }
 
@@ -48,24 +48,86 @@ function enemyResult(){
 }
 
 
+let count =0;
+
+let playerScore = 0;
+let compScore = 0;
+
+function countGoup(){
+     count++;
+}
+
+
+
+
 function compareFunction(){
+     
+    
+     
+     
+    
+     function checker(){
+         
+          function score1(){
+               console.log("score");
+               if (playerScore > compScore){
+                   txtbocks.innerText = "Winner!"
+                   winaudio.play(); 
+               }
+               else if(compScore > playerScore){
+                    txtbocks.innerText = "Loser!"
+                    loseraudio.play();
+               }
+               else if(compScore === playerScore){
+                    txtbocks.innerText = "Tie Game!"
+               }
+     
+               
+          }
+
+
+
+
+
+
+
+          if (count === 5){
+              
+               score1()
+          }
+     }
+    
+    
+     
+     
+     
+     
+     
+     checker();
+     
+     
+     
+   
      if (playerInput == "rock" && rresult == "Scissors" || 
          playerInput == "paper" && rresult =="Rock"||
          playerInput == "scissors" && rresult =="Paper"){
-          winaudio.play();
-          txtbocks.innerText = "You Win!"
+              console.log("player round");
+          
+          playerScore++;
      }
      else if (playerInput =="rock" && rresult =="Rock" ||
               playerInput =="paper" && rresult == "Paper" ||
               playerInput =="scissors" && rresult == "Scissors"){
-               txtbocks.innerText = "Tie Game!"
+               console.log("draw round");
     
      }
      else if (playerInput=="rock" && rresult =="Paper"||
               playerInput=="paper" && rresult =="Scissors"||
               playerInput=="scissors" && rresult =="Rock"){
-               txtbocks.innerText = "You Lose!"
-              }
+               console.log("computer round");
+               compScore++;
+              };
+              
 }
    
 // sound effects
@@ -73,6 +135,7 @@ const paperaudio = new Audio("paper.wav");
 const rockaudio = new Audio("rock.wav");
 const scissoraudio = new Audio("scissors.wav")
 const winaudio = new Audio("cheer.wav")
+const loseraudio = new Audio("loser.wav")
 
 
 const btn1 = document.querySelector("#b1")
@@ -115,8 +178,8 @@ btn3.addEventListener('click', () => {
 });
 
 playbtn.addEventListener("click", () => {
-     count++;
-     checker()
+     
+     countGoup()
      computerPlay()
      compareFunction()
      enemyResult()
